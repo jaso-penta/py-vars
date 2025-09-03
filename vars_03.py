90ab38b6ca32b4d5736a28d3f1a491579361d631
Jašo <andrija.pericic@gmail.com>
2025-09-03 18:53:10 +0200

    Create vars_03.py

---
+++ vars_03.py
@@ -0,0 +1,42 @@
+# Ovo je komentar
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjene proizvoljne dužine trokuta.
+'''
+a = 10
+b = 5
+c = 5
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjeni podaci o filmu: naziv filma, 
+godina proizvodnje, proječna ocjena, kratki opis
+'''
+movie_tittle = 'Memento'
+release_date = 2000
+imdb = 8.4
+summary = "Reviewers say 'Memento' is acclaimed for its inventive narrative and exploration of memory and identity. Guy Pearce's compelling performance and Christopher Nolan's direction are highly praised. The film's technical elements, such as cinematography and editing, receive significant recognition. However, some find its complexity and non-linear structure demanding. Despite this, 'Memento' is often seen as a rewarding and thought-provoking experience that benefits from multiple viewings."
+
+'''
+Potrebno je kreirati variajble u kojima će biti pohranjeni podaci o imenu i prezimenu studenta, 
+završnoj ocjeni te podataka je li diplomirao ili nije
+'''
+
+student_name = 'Ivo'
+student_surname = 'Ivic'
+grade = 4.4
+graduate = True
+
+'''
+Kreirati varijablu za čuvanje podataka o studentu: ime i prezime, OIB, email, 
+broj telefona, prosječna ocjena, godina upisa studija, naziv studija, naziv sveučilišta
+'''
+
+student_name = "Ivo"
+student_surname = "Ivic"
+oib = 12345678900
+email = x@gmail.com
+phone_number = 09555544433
+average_grade = 5.0
+year_of_enrollment = 2000
+study_field = "Agronomija"
+university = "Sveuciliste u Zadru"
